b704e9158000b84e7a42ca1378dde94f638d7a38
ucupsmith <akilnauffancozz@gmail.com>
2023-12-03 21:23:06 +0700

    ngubah logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,17 +13,17 @@
                 </div>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="img/carousell.png" class="d-block w-100" alt="...">
+                    <img src="img/image 31.png" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="img/carousell.png" class="d-block w-100" alt="...">
+                    <img src="img/image 31.png" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="img/carousell.png" class="d-block w-100" alt="...">
+                    <img src="img/image 31.png" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                     </div>
                   </div>
@@ -45,49 +45,139 @@
         <h5>Feature Events</h5>
         <div class="col">
             
-            <div class="card my-4" style="width:300px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                  <h4 class="card-title">John Doe</h4>
-                  <p class="card-text">Some example text.</p>
-                  <a href="#" class="btn btn-primary">See Profile</a>
-                </div>
+          <div class="card my-4" style="width:300px">
+              <img class="card-img-top c-img" src="img/image 31.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+                <p class="card-text">10 dec 2023 </p>
+                <p class="card-text">Rp 1.294.000 </p>
+                <img src="img/R.png" alt="" class="position-absolute" style="text-transform:capitalize; width:30px; top:44vh; left: 2.5vw;">
+                <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+                
               </div>
-        </div>
+            </div>
 
-        <div class="col">
-            <div class="card my-4" style="width:300px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                  <h4 class="card-title">John Doe</h4>
-                  <p class="card-text">Some example text.</p>
-                  <a href="#" class="btn btn-primary">See Profile</a>
-                </div>
-              </div>
-        </div>
+      </div>
 
-        <div class="col">
-            <div class="card my-4" style="width:300px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                  <h4 class="card-title">John Doe</h4>
-                  <p class="card-text">Some example text.</p>
-                  <a href="#" class="btn btn-primary">See Profile</a>
-                </div>
-              </div>
-        </div>
 
-        <div class="col">
-            <div class="card my-4" style="width:300px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                  <h4 class="card-title">John Doe</h4>
-                  <p class="card-text">Some example text.</p>
-                  <a href="#" class="btn btn-primary">See Profile</a>
-                </div>
-              </div>
+      <div class="col">
+
+        <div class="card my-4" style="width:300px">
+          <img class="card-img-top c-img" src="img/image 31.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+            <p class="card-text">10 dec 2023 </p>
+            <p class="card-text">Rp 1.294.000 </p>
+            <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+            <img src="img/R.png" alt="" class="position-absolute" style="text-transform:capitalize; width:30px; top:44vh; left: 2.5vw;">
+          </div>
         </div>
+      </div>
+
+      <div class="col">
+
+        <div class="card my-4" style="width:300px">
+          <img class="card-img-top c-img" src="img/image 31.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+            <p class="card-text">10 dec 2023 </p>
+            <p class="card-text">Rp 1.294.000 </p>
+            <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+            <img src="img/R.png" alt="" class="position-absolute" style="text-transform:capitalize; width:30px; top:44vh; left: 2.5vw;">
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+
+        <div class="card my-4" style="width:300px">
+          <img class="card-img-top c-img" src="img/image 31.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+            <p class="card-text">10 dec 2023 </p>
+            <p class="card-text">Rp 1.294.000 </p>
+            <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+            <img src="img/R.png" alt="" class="position-absolute" style="text-transform:capitalize; width:30px; top:44vh; left: 2.5vw;">
+          </div>
+        </div>
+      
+      </div>
         
+    </div>
+
+    
+    <div class="row">
+      <h5>festifal fair</h5>
+
+      <div class="col">
+        
+         <div class="card my-4" style="width:300px">
+                <img class="card-img-top c-img" src="img/carousell.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+                  <p class="card-text">10 dec 2023 </p>
+                  <p class="card-text">Rp 1.294.000 </p>
+                  <a href="#" class="btn border-top w-100 ">Prestige Promotion</a>
+                  <img src="img/R.png" alt="" class="card-img">
+                </div>
+              </div>
+
+        </div>
+
+
+        <div class="col">
+
+          <div class="card my-4" style="width:300px">
+            <img class="card-img-top c-img" src="img/carousell.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+              <p class="card-text">10 dec 2023 </p>
+              <p class="card-text">Rp 1.294.000 </p>
+              <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+              <img src="img/R.png" alt="" class="card-img">
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+
+          <div class="card my-4" style="width:300px">
+            <img class="card-img-top c-img" src="img/carousell.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+              <p class="card-text">10 dec 2023 </p>
+              <p class="card-text">Rp 1.294.000 </p>
+              <a href="#" class="btn border-top w-100">Prestige Promotion</a>
+              <img src="img/R.png" alt="" class="card-img">
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+
+          <div class="card my-4" style="width:300px">
+            <img class="card-img-top c-img" src="img/carousell.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title" style="text-transform: capitalize">steve aoki's</h4>
+              <p class="card-text">10 dec 2023 </p>
+              <p class="card-text">Rp 1.294.000 </p>
+              <a href="#" class="btn border-top w-100 h-100">Prestige Promotion</a>
+              <img src="img/R.png" alt="" class="card-img">
+            </div>
+          </div>
+
+        </div>
+
+        
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="container">
+          <div class="btn btn-success my-4 ">explore all the events</div>
+        </div>
+      </div>
     </div>
     
 
