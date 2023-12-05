d1ea4b53193837a3c3e68acf105514a5b3a93c32
Walid Fernando Sastriana <walidfernando21@gmail.com>
2023-12-05 20:19:49 +0700

    Add Register page and Update Sign In

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,7 +1,23 @@
-@extends('layouts.layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
 
-@section('content')
+    <link rel="stylesheet" href="style.css">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
     <div class="container">
         <h2 class="card-title text-center mb-4 text-capitalize">Sign In</h2>
         <form action="#">
@@ -30,5 +46,5 @@
         </form>
     </div>
 
-@endsection
+</body>
 
