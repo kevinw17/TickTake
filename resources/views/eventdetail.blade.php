8d8968d7d8640685d5ce92a5328c3ca6d1a37c4d
ucupsmith <akilnauffancozz@gmail.com>
2023-12-10 10:54:48 +0700

    update eventdetail_belom selesai

--- resources/views/eventdetail.blade.php
+++ resources/views/eventdetail.blade.php
@@ -5,8 +5,8 @@
     <div class="container-fluid py-4">
         <div class="container">
             <div class="row">
-                <div class="col-lg-5 col-md-12 col-12">
-                    <img src="img/image 31.png" alt="" class="img-fluid h-75">
+                <div class="col-lg-5 col-md-4 col-12">
+                    <img src="img/image 31.png" alt="" class="img-fluid d-fixed h-75">
 
                     <div class="col-lg-12 d-flex mt-1" style="justify-content: space-between">
                         <div class="col-md-3 px-2 py-1">
@@ -32,9 +32,9 @@
                 <div class="col-lg-7 col-md-12 col-12">
                     <h1 class="justify-content-start text-center text-capitalize border py-4">fifa word cup-17</h1>
                     <div class="col-md-12 col-4 border my-2">
-                        <p class="text-header">date</p>
-                        <p class="text-header">time</p>
-                        <p class="text-header">place</p>
+                        <p class="text-header">date :</p>
+                        <p class="text-header">time :</p>
+                        <p class="text-header">place :</p>
                     </div>
                     <div class="col-md-12 justify-content-center border">
                         <i class="fa-solid fa-star fa-2x" style="color: tomato;"></i>
@@ -55,11 +55,46 @@
                         </div>
                     </div>
 
-                    <div class="col-md-6 d-inline me-12">
-                        <button class="btn btn-success text-capitalize me-4"> buy ticket</button>
+                
+                    {{-- <div class="col-md-12 d-flex border">
+                        <button class="btn btn-success d-lg-inline-flex" >
+                            click here
+                        </button>
+                    </div> --}}
+                    
+                    <div class="col-lg-12">
+                        <h5 class="text-title text-capitalize">high lights</h5>
+  
+                        <ul class="content">
+                            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod autem deserunt repellat doloribus quibusdam similique rem id alias ut officiis? Exercitationem quia facere nihil ad velit, earum ipsa minus itaque.</li>
+                            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis velit autem non reprehenderit, voluptatum quos sit amet fugit minima eum consectetur ad dolorem odit! Ea adipisci quidem magnam dolorum velit?</li>
+                            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, modi tempora corporis sed inventore beatae repudiandae vero ipsa quasi sit minus perferendis architecto eaque dolor illo velit, aspernatur soluta repellendus.</li>
+
+                        </ul>
+
+                        <div class="col-md-6 border">
+                            <div class="content">
+
+                                <i class="fas fa-announcement"></i>
+                                <h5 class="text-title text-capitalize mt-0">important info</h5>
+                                
+                                <ul>
+                                    <li>hi</li>
+                                    <li>hi</li>
+                                    <li>hi</li>
+                                </ul>
+
+                            </div>
+                        </div>
+    
                     </div>
 
+                
+                    
+
                 </div>
+
+               
 
             </div>
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -18,8 +18,18 @@
 </head>
 <body>
 
-    <div class="container">
-        <h2 class="card-title text-center mb-4 text-capitalize">Register</h2>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-4 col-12 " style="margin-top:14vh; ">
+                    <h2 class="card-title text-center mb-4 text-capitalize ">Register</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-flex align-items-center justify-content-center" style="margin-top:4vh;">
+        
         <form action="#">
 
             <div class="form-group">
@@ -44,7 +54,7 @@
                     </a></p>
             </div>
             <div class="col">
-                <button class="btn btn-outline-primary col-md-7 card-title text-center mb-4" style="background-color: #4ECDC4">Register</button>
+                <button class="btn btn-outline-primary col-md-7 card-title text-center mb-4 w-100" style="background-color: #4ECDC4">Register</button>
             </div>
             
         </form>
